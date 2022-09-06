c25da97559cf881c6302b6b906c84d898d3bbafc
Esma <esmazuurbier12@gmail.com>
2022-09-06 12:34:08 +0200

    added functions to control recoil animation

--- include/gun.h
+++ include/gun.h
@@ -29,15 +29,43 @@ void walkingMotion(float &gunY, float &gunZ, float currentFrame);
 void drawhandGun(Model gun, Shader &shader);
 
 /**
- * @brief calculates the model matrix for the gun and sets all the uniforms for the handgun shader.
+ * @brief Get the gun model matrix.
  * 
- * @param[out] shader shader for gun model.
- * @param gunPosition position vector for gun.
+ * @param gunPosition position vector of gun.
  * @param cameraView camera view matrix.
- * @param scaleFactor make gun smaller/larger by a certain factor.
- * @param rotation y-axis rotation (counter clockwise).
+ * @param scaleFactor make gun larger/smaller by this factor.
+ * @param rotation y-axis counter clockwise rotation (in degrees)
+ * @return glm::mat4 gun model matrix
  */
-void configureGunShader(Shader &shader,  glm::vec3 gunPosition, glm::mat4 cameraView, float scaleFactor, float rotation);
+glm::mat4 getGunModelMatrix(glm::vec3 gunPosition, glm::mat4 cameraView, float scaleFactor, float rotation);
+
+/**
+ * @brief first part recoil animation: gun rotates up untill the angle has reached a certain value.
+ * 
+ * @param[out] shader shader for gun.
+ * @param[out] modelMatrixGun model matrix of gun (output of getGunModelMatrix).
+ * @param[out] angle x - axis rotation angle (upwards). The angle will be increased a little with every frame.
+ * @param[out] down does gun need to rotate down? (should be false in this context). This will be set to true
+ * if the angle has reached a certain value.
+ * @see getGunModelMatrix().
+ */
+void startRecoilAnimation(Shader &shader, glm::mat4 &modelMatrixGun, float &angle, bool &down);
+
+/**
+ * @brief second part of recoil animation: gun rotates down, back to base position. The recoil animation
+ * has finished when the gun is back to base position: x-axis rotation is back to zero.
+ * 
+ * @param[out] shader shader for gun.
+ * @param[out] modelMatrixGun model matrix of gun (output of getGunModelMatrix).
+ * @param[out] angle x - axis rotation angle (downwards). The angle will be decreased a little with every frame and will be set back to 
+ * zero after the recoil animation has finished.
+ * @param[out] shot has player takes a shot? This will be set back to false after the recoil animation has finished.
+ * @param[out] down does gun need to rotate down? This should be true in this context and it will be set back to
+ * false after recoil animation has finished.
+ * @param[out] startRecoil start the recoil animation? This should be true in this context and it will be set back to false
+ * after the recoil animation has finished. 
+ */
+void goBackToBase(Shader &shader, glm::mat4 &modelMatrixGun, float &angle, bool &shot, bool &down, bool &startRecoil);
 
 #endif /*__GUN__*/
 
--- src/gun.cpp
+++ src/gun.cpp
@@ -14,15 +14,45 @@ void drawhandGun(Model gun, Shader &shader)
     gun.drawSpecificMesh(shader, 6);
 }
 
-void configureGunShader(Shader &shader, glm::vec3 gunPosition, glm::mat4 cameraView, float scaleFactor, float rotation)
+glm::mat4 getGunModelMatrix(glm::vec3 gunPosition, glm::mat4 cameraView, float scaleFactor, float rotation)
 {
     glm::mat4 gunModel = glm::mat4(1.0); 
     gunModel = glm::translate(gunModel, gunPosition); // place gun in bottom right corner
     gunModel = glm::rotate(gunModel, glm::radians(rotation), glm::vec3(0.0f, 1.0f, 0.0f)); // rotate gun so it points inwards
-    //float angle = glfwGetTime();
-    //std::cout << angle << std::endl;
-    //gunModel = glm::rotate(gunModel, glm::radians(angle), glm::vec3(0.0f,  0.0f,1.0f)); 
     gunModel = glm::scale(gunModel, glm::vec3(scaleFactor)); // make gun a bit smaller
-    shader.setMat4("view", cameraView); 
-    shader.setMat4("model", glm::inverse(cameraView) * gunModel);
+    gunModel = glm::inverse(cameraView) * gunModel;
+
+    return gunModel;
+}
+
+void startRecoilAnimation(Shader &shader, glm::mat4 &modelMatrixGun, float &angle, bool &down)
+{
+    if (angle > 20.0f) // rotate up as long as angle has not surpassed this value
+    {
+        down = true;
+    }
+
+    modelMatrixGun = glm::rotate(modelMatrixGun, glm::radians(angle), glm::vec3(0.0f,  0.0f, 1.0f)); // rotate upwards
+    shader.setMat4("model", modelMatrixGun);
+    
+    angle+=6.0; // increase angle with every frame
+}
+
+void goBackToBase(Shader &shader, glm::mat4 &modelMatrixGun, float &angle, bool &shot, bool &down, bool &startRecoil)
+{
+    if (angle > 0) // rotate down as long as base position is not reached
+    {
+        modelMatrixGun = glm::rotate(modelMatrixGun, glm::radians(angle), glm::vec3(0.0f,  0.0f, 1.0f)); // rotate downwards
+        shader.setMat4("model", modelMatrixGun);
+    }
+
+    if (angle <= 0) // set all values back to what they were before player shot gun
+    {
+        angle = 0;
+        shot = false;
+        down = false;
+        startRecoil = false;
+    }
+
+    angle-=6.0; // decrease angle with every frame
 }
