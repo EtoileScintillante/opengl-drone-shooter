f570c2cb8a627d212dbc76a74de3537bfdfa99d0
Esma <esmazuurbier12@gmail.com>
2022-09-07 17:03:59 +0200

    changes in constructor variables

--- include/mobs.h
+++ include/mobs.h
@@ -29,8 +29,10 @@ public:
     std::vector < glm::vec3 > treePositions;
     std::string texturePath;
     unsigned int terrainSize;
+    float groundY;
     float blockSize;
-    bool isShot;
+    float minHeight;
+    float maxHeight;
     float currentTime; // needed to play animation
 
     /**
@@ -38,41 +40,48 @@ public:
      * 
      * @param verticesZombie vertex data for zombie block.
      * @param verticesCreeper vertex data for creeper block.
-     * @param treePositions tree positions (to make sure that a mob does not spawn inside a tree).
-     * @param texturePath path to texture
-     * @param terrainSize terrain size. Note: the actual size of the terrain is calculated by terrainSize * terrainSize
+     * @param treePositions tree positions (needed to avoid mobs spawning inside a tree)
+     * @param texturePath path to mobs texture.
+     * @param terrainSize size of terrain. Note: the actual size of the terrain is calculated by terrainSize * terrainSize
      * (the terrain is shaped like a square), the input variable here should correspond to the amount of blocks that make up one side of the square).
-     * @param blockSize size of mob block
-     * @param isShot has a mob been hit by player?
+     * @param groundY y value of ground.
+     * @param blockSize size of a block.
+     * @param minHeight minimum y value for floating position of mob.
+     * @param maxheight maximum y value for floating position of mob.
      */
-    Mobs(std::vector < Data > verticesZombie, std::vector < Data > verticesCreeper, std::vector < glm::vec3 > treePositions, std::string texturePath, unsigned int terrainSize, float blockSize, bool isShot);
+    Mobs(std::vector < Data > verticesZombie, std::vector < Data > verticesCreeper, std::vector < glm::vec3 > treePositions, std::string texturePath, unsigned int terrainSize, float groundY, float blockSize, float minHeight, float maxHeight);
 
     /**
      * @brief draws a mob.
      * 
      * @param shader shader for mob blocks.
-     * @param currentTime current frame.
+     * @param cameraView camera view matrix.
+     * @param projection projection matrix.
      */
-    void Spawn(Shader &shader, float currentFrame);
+    void Spawn(Shader &shader, glm::mat4 cameraView, glm::mat4 projection);
 
+    // TODO
     void collisionDetection();
 
 private:
     unsigned int texture = 6;
     unsigned int VAOcreeper, VAOzombie;
     unsigned int VBOcreeper, VBOzombie;
-    glm::vec3 currentPosition;
-    std::string currentMob; // has to be chosen in configrureMobs and changed in dyingAnimation if mob has been shot
+    glm::vec3 currentPosition; // has to be chosen in configrureMobs and changed in dyingAnimation if mob has been shot
+    unsigned int currentMob; // has to be chosen in configrureMobs and changed in dyingAnimation if mob has been shot
+    bool isShot;
 
     /// sets up and configures buffers/arrays
     void configureMobs();
 
-    /// @brief  generates a random spawn position.
-    /// @return vector containing random positions.
-    std::vector < glm::vec3 > randomPositions();
-
     /// play little animation when mob has been killed.
     void dyingAnimation();
+
+    /// generates a random position
+    void getRandomPos();
+
+    /// chooses a mob (zombie or creeper).
+    void chooseMob();
 };
 
 #endif /*__MOBS__*/
