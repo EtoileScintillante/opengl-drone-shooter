a15cbbd704e6b481b8ac9b969eefb81a1475b54d
Esma <esmazuurbier12@gmail.com>
2022-09-11 14:45:56 +0200

    separated definitions and implementations

--- include/model.h
+++ include/model.h
@@ -38,175 +38,26 @@ public:
     bool gammaCorrection;
 
     /// constructor, expects a filepath to a 3D model.
-    Model(std::string const &path, bool gamma = false) : gammaCorrection(gamma)
-    {
-        loadModel(path);
-    }
+    Model(std::string const &path, bool gamma = false);
 
     /// draws the model, and thus all its meshes
-    void Draw(Shader &shader)
-    {
-        for(unsigned int i = 0; i < meshes.size(); i++)
-            meshes[i].Draw(shader);
-    }
+    void Draw(Shader &shader);
 
     /// only draws mesh at given index
-    void drawSpecificMesh(Shader &shader, int index)
-    {
-        meshes[index].Draw(shader);
-    }
+    void drawSpecificMesh(Shader &shader, int index);
     
 private:
     /// loads a model with supported ASSIMP extensions from file and stores the resulting meshes in the meshes vector.
-    void loadModel(std::string const &path)
-    {
-        // read file via ASSIMP
-        Assimp::Importer importer; // declare importer object
-        const aiScene* scene = importer.ReadFile(path, aiProcess_Triangulate | aiProcess_GenSmoothNormals | aiProcess_FlipUVs | aiProcess_CalcTangentSpace);
-        // check for errors (check if the scene and root node of the scene are not null and check one of its flags to see if the returned data is incomplete)
-        if(!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode) // if is Not Zero
-        {
-            std::cout << "ERROR::ASSIMP:: " << importer.GetErrorString() << std::endl;
-            return;
-        }
-        // retrieve the directory path of the filepath
-        directory = path.substr(0, path.find_last_of('/'));
-
-        // process ASSIMP's root node recursively if no errors occured
-        processNode(scene->mRootNode, scene);
-    }
+    void loadModel(std::string const &path);
 
     /// processes a node in a recursive fashion. Processes each individual mesh located at the node and repeats this process on its children nodes (if any).
-    void processNode(aiNode *node, const aiScene *scene)
-    {
-        // process each mesh located at the current node
-        for(unsigned int i = 0; i < node->mNumMeshes; i++)
-        {
-            aiMesh* mesh = scene->mMeshes[node->mMeshes[i]]; 
-            meshes.push_back(processMesh(mesh, scene));
-        }
-        // after we've processed all of the meshes (if any) we then recursively process each of the children nodes
-        for(unsigned int i = 0; i < node->mNumChildren; i++)
-        {
-            processNode(node->mChildren[i], scene);
-        }
-
-    }
+    void processNode(aiNode *node, const aiScene *scene);
 
     /// processes the data of a mesh
-    Mesh processMesh(aiMesh *mesh, const aiScene *scene)
-    {
-        // data to fill
-        std::vector<Vertex> vertices;
-        std::vector<unsigned int> indices;
-        std::vector<Texture> textures;
-
-        // walk through each of the mesh's vertices
-        for(unsigned int i = 0; i < mesh->mNumVertices; i++)
-        {
-            Vertex vertex;
-            glm::vec3 vector; // we declare a placeholder vector since assimp uses its own vector class that doesn't directly convert to glm's vec3 class so we transfer the data to this placeholder glm::vec3 first.
-            // positions
-            vector.x = mesh->mVertices[i].x; // Assimp calls their vertex position array mVertices
-            vector.y = mesh->mVertices[i].y;
-            vector.z = mesh->mVertices[i].z;
-            vertex.Position = vector;
-            // normals
-            if (mesh->HasNormals())
-            {
-                vector.x = mesh->mNormals[i].x;
-                vector.y = mesh->mNormals[i].y;
-                vector.z = mesh->mNormals[i].z;
-                vertex.Normal = vector;
-            }
-            // texture coordinates
-            if(mesh->mTextureCoords[0]) // does the mesh contain texture coordinates?
-            {
-                glm::vec2 vec;
-                vec.x = mesh->mTextureCoords[0][i].x; 
-                vec.y = mesh->mTextureCoords[0][i].y;
-                vertex.TexCoords = vec;
-                // tangent
-                vector.x = mesh->mTangents[i].x;
-                vector.y = mesh->mTangents[i].y;
-                vector.z = mesh->mTangents[i].z;
-                vertex.Tangent = vector;
-                // bitangent
-                vector.x = mesh->mBitangents[i].x;
-                vector.y = mesh->mBitangents[i].y;
-                vector.z = mesh->mBitangents[i].z;
-                vertex.Bitangent = vector;
-            }
-            else
-                vertex.TexCoords = glm::vec2(0.0f, 0.0f);
-
-            vertices.push_back(vertex);
-        }
-       
-        for(unsigned int i = 0; i < mesh->mNumFaces; i++)
-        {
-            aiFace face = mesh->mFaces[i];
-            for(unsigned int j = 0; j < face.mNumIndices; j++)
-                indices.push_back(face.mIndices[j]);        
-        }
-        // process materials
-        aiMaterial* material = scene->mMaterials[mesh->mMaterialIndex]; // retrieve the aiMaterial object from the scene's mMaterials array 
-        
-        /* We assume that the textures are named in the sampler as follows:
-        diffuse: texture_diffuseN
-        specular: texture_specularN
-        normal: texture_normalN
-        Where N is a sequential number ranging from 1 to MAX_SAMPLER_NUMBER. */
-
-        // 1. diffuse maps
-        std::vector<Texture> diffuseMaps = loadMaterialTextures(material, aiTextureType_DIFFUSE, "texture_diffuse");
-        textures.insert(textures.end(), diffuseMaps.begin(), diffuseMaps.end());
-        // 2. specular maps
-        std::vector<Texture> specularMaps = loadMaterialTextures(material, aiTextureType_SPECULAR, "texture_specular");
-        textures.insert(textures.end(), specularMaps.begin(), specularMaps.end());
-        // 3. normal maps
-        std::vector<Texture> normalMaps = loadMaterialTextures(material, aiTextureType_HEIGHT, "texture_normal");
-        textures.insert(textures.end(), normalMaps.begin(), normalMaps.end());
-        // 4. height maps
-        std::vector<Texture> heightMaps = loadMaterialTextures(material, aiTextureType_AMBIENT, "texture_height");
-        textures.insert(textures.end(), heightMaps.begin(), heightMaps.end());
-        
-        // return a mesh object created from the extracted mesh data
-        return Mesh(vertices, indices, textures);
-    }
+    Mesh processMesh(aiMesh *mesh, const aiScene *scene);
 
     /// checks all material textures of a given type and loads the textures if they're not loaded yet. The required info is returned as a Texture struct.
-    std::vector<Texture> loadMaterialTextures(aiMaterial *mat, aiTextureType type, std::string typeName)
-    {
-        std::vector<Texture> textures;
-        for(unsigned int i = 0; i < mat->GetTextureCount(type); i++)
-        {
-            aiString str; // stores texture path
-            mat->GetTexture(type, i, &str); // retrieve the texture's file location
-            // check if texture was loaded before and if so, continue to next iteration: skip loading a new texture
-            bool skip = false;
-            for(unsigned int j = 0; j < textures_loaded.size(); j++)
-            {
-                if(std::strcmp(textures_loaded[j].path.data(), str.C_Str()) == 0)
-                {
-                    textures.push_back(textures_loaded[j]);
-                    skip = true; // a texture with the same filepath has already been loaded, continue to next one. (optimization)
-                    break;
-                }
-            }
-            if(!skip)
-            {   // if texture hasn't been loaded already, load it
-                Texture texture;
-                // note that we make the assumption that texture file paths in model files are local to the actual model object e.g. in the same directory as the location of the model itself. 
-                texture.id = TextureFromFile(str.C_Str(), this->directory);
-                texture.type = typeName;
-                texture.path = str.C_Str();
-                textures.push_back(texture);
-                textures_loaded.push_back(texture);  // store it as texture loaded for entire model, to ensure we won't unnecesery load duplicate textures.
-            }
-        }
-        return textures;
-    }
+    std::vector<Texture> loadMaterialTextures(aiMaterial *mat, aiTextureType type, std::string typeName);
 };
 
 #endif /*__MODEL__*/
--- include/shader.h
+++ include/shader.h
@@ -22,94 +22,13 @@ public:
     unsigned int ID;
 
     /// default constructor
-    Shader() = default;
+    Shader();
 
     /// constructor (it generates the shader on the fly)
-    Shader(const char* vertexPath, const char* fragmentPath, const char* geometryPath = nullptr)
-    {
-        // 1. retrieve the vertex/fragment source code from filePath
-        std::string vertexCode;
-        std::string fragmentCode;
-        std::string geometryCode;
-        std::ifstream vShaderFile;
-        std::ifstream fShaderFile;
-        std::ifstream gShaderFile;
-        // ensure ifstream objects can throw exceptions:
-        vShaderFile.exceptions (std::ifstream::failbit | std::ifstream::badbit);
-        fShaderFile.exceptions (std::ifstream::failbit | std::ifstream::badbit);
-        gShaderFile.exceptions (std::ifstream::failbit | std::ifstream::badbit);
-        try 
-        {
-            // open files
-            vShaderFile.open(vertexPath);
-            fShaderFile.open(fragmentPath);
-            std::stringstream vShaderStream, fShaderStream;
-            // read file's buffer contents into streams
-            vShaderStream << vShaderFile.rdbuf();
-            fShaderStream << fShaderFile.rdbuf();		
-            // close file handlers
-            vShaderFile.close();
-            fShaderFile.close();
-            // convert stream into string
-            vertexCode = vShaderStream.str();
-            fragmentCode = fShaderStream.str();	
-            // if geometry shader path is present, also load a geometry shader
-            if(geometryPath != nullptr)
-            {
-                gShaderFile.open(geometryPath);
-                std::stringstream gShaderStream;
-                gShaderStream << gShaderFile.rdbuf();
-                gShaderFile.close();
-                geometryCode = gShaderStream.str();
-            }		
-        }
-        catch (std::ifstream::failure& e)
-        {
-            std::cout << "ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ: " << e.what() << std::endl;
-        }
-        const char* vShaderCode = vertexCode.c_str();
-        const char * fShaderCode = fragmentCode.c_str();
-        // 2. compile shaders
-        unsigned int vertex, fragment;
-        // vertex shader
-        vertex = glCreateShader(GL_VERTEX_SHADER);
-        glShaderSource(vertex, 1, &vShaderCode, NULL);
-        glCompileShader(vertex);
-        checkCompileErrors(vertex, "VERTEX");
-        // fragment Shader
-        fragment = glCreateShader(GL_FRAGMENT_SHADER);
-        glShaderSource(fragment, 1, &fShaderCode, NULL);
-        glCompileShader(fragment);
-        checkCompileErrors(fragment, "FRAGMENT");
-        unsigned int geometry;
-        if(geometryPath != nullptr)
-        {
-            const char * gShaderCode = geometryCode.c_str();
-            geometry = glCreateShader(GL_GEOMETRY_SHADER);
-            glShaderSource(geometry, 1, &gShaderCode, NULL);
-            glCompileShader(geometry);
-            checkCompileErrors(geometry, "GEOMETRY");
-        }
-        // shader Program
-        ID = glCreateProgram();
-        glAttachShader(ID, vertex);
-        glAttachShader(ID, fragment);
-        if(geometryPath != nullptr)
-            glAttachShader(ID, geometry);
-        glLinkProgram(ID);
-        checkCompileErrors(ID, "PROGRAM");
-        // delete the shaders as they're linked into our program now and no longer necessery
-        glDeleteShader(vertex);
-        glDeleteShader(fragment);
-        if(geometryPath != nullptr)
-            glDeleteShader(geometry);
+    Shader(const char* vertexPath, const char* fragmentPath, const char* geometryPath = nullptr);
 
-    }
     /// activates the shader
-    void use() const
-    { 
-        glUseProgram(ID); 
-    }
+    void use() const;
     
     /**
      * @brief Set bool uniform.
@@ -117,10 +36,7 @@ public:
      * @param name name of uniform.
      * @param value bool.
      */
-    void setBool(const std::string &name, bool value) const
-    {         
-        glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value); 
-    }
+    void setBool(const std::string &name, bool value) const;
 
     /**
      * @brief Set int uniform.
@@ -128,10 +44,7 @@ public:
      * @param name name of uniform.
      * @param value int.
      */
-    void setInt(const std::string &name, int value) const
-    { 
-        glUniform1i(glGetUniformLocation(ID, name.c_str()), value); 
-    }
+    void setInt(const std::string &name, int value) const;
     
     /**
      * @brief Set float uniform.
@@ -139,10 +52,7 @@ public:
      * @param name name of uniform.
      * @param value float.
      */
-    void setFloat(const std::string &name, float value) const
-    { 
-        glUniform1f(glGetUniformLocation(ID, name.c_str()), value); 
-    }
+    void setFloat(const std::string &name, float value) const;
     
     /**
      * @brief Set vec2 uniform: glm::vec2.
@@ -150,10 +60,7 @@ public:
      * @param name name of uniform.
      * @param value glm::vec2.
      */
-    void setVec2(const std::string &name, const glm::vec2 &value) const
-    { 
-        glUniform2fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]); 
-    }
+    void setVec2(const std::string &name, const glm::vec2 &value) const;
 
     /**
      * @brief Set vec2 uniform: x, y.
@@ -162,10 +69,7 @@ public:
      * @param x x.
      * @param y y.
      */
-    void setVec2(const std::string &name, float x, float y) const
-    { 
-        glUniform2f(glGetUniformLocation(ID, name.c_str()), x, y); 
-    }
+    void setVec2(const std::string &name, float x, float y) const;
     
     /**
      * @brief Set vec3 uniform: glm::vec3.
@@ -173,10 +77,7 @@ public:
      * @param name name of uniform.
      * @param value glm::vec3.
      */
-    void setVec3(const std::string &name, const glm::vec3 &value) const
-    { 
-        glUniform3fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]); 
-    }
+    void setVec3(const std::string &name, const glm::vec3 &value) const;
 
     /**
      * @brief Set vec3 uniform: x, y, z.
@@ -186,10 +87,7 @@ public:
      * @param y y.
      * @param z z.
      */
-    void setVec3(const std::string &name, float x, float y, float z) const
-    { 
-        glUniform3f(glGetUniformLocation(ID, name.c_str()), x, y, z); 
-    }
+    void setVec3(const std::string &name, float x, float y, float z) const;
     
     /**
      * @brief Set vec4 uniform: glm::vec4.
@@ -197,10 +95,7 @@ public:
      * @param name name of uniform.
      * @param value glm::vec4.
      */
-    void setVec4(const std::string &name, const glm::vec4 &value) const
-    { 
-        glUniform4fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]); 
-    }
+    void setVec4(const std::string &name, const glm::vec4 &value) const;
 
     /**
      * @brief Set vec4 uniform: x, y, z, w.
@@ -211,10 +106,7 @@ public:
      * @param z z.
      * @param w w.
      */
-    void setVec4(const std::string &name, float x, float y, float z, float w) const
-    { 
-        glUniform4f(glGetUniformLocation(ID, name.c_str()), x, y, z, w); 
-    }
+    void setVec4(const std::string &name, float x, float y, float z, float w) const;
     
     /**
      * @brief Set mat2 uniform.
@@ -222,10 +114,7 @@ public:
      * @param name name of uniform.
      * @param mat glm::mat2.
      */
-    void setMat2(const std::string &name, const glm::mat2 &mat) const
-    {
-        glUniformMatrix2fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
-    }
+    void setMat2(const std::string &name, const glm::mat2 &mat) const;
 
     /**
      * @brief Set mat3 uniform.
@@ -233,10 +122,7 @@ public:
      * @param name name of uniform.
      * @param mat glm::mat3.
      */
-    void setMat3(const std::string &name, const glm::mat3 &mat) const
-    {
-        glUniformMatrix3fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
-    }
+    void setMat3(const std::string &name, const glm::mat3 &mat) const;
     
     /**
      * @brief Set mat4 uniform.
@@ -244,35 +130,10 @@ public:
      * @param name name of uniform.
      * @param mat glm::mat4.
      */
-    void setMat4(const std::string &name, const glm::mat4 &mat) const
-    {
-        glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
-    }
-
+    void setMat4(const std::string &name, const glm::mat4 &mat) const;
+    
 private:
     /// utility function for checking shader compilation/linking errors.
-    void checkCompileErrors(GLuint shader, std::string type)
-    {
-        GLint success;
-        GLchar infoLog[1024];
-        if (type != "PROGRAM")
-        {
-            glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
-            if (!success)
-            {
-                glGetShaderInfoLog(shader, 1024, NULL, infoLog);
-                std::cout << "ERROR::SHADER_COMPILATION_ERROR of type: " << type << "\n" << infoLog << "\n -- --------------------------------------------------- -- " << std::endl;
-            }
-        }
-        else
-        {
-            glGetProgramiv(shader, GL_LINK_STATUS, &success);
-            if (!success)
-            {
-                glGetProgramInfoLog(shader, 1024, NULL, infoLog);
-                std::cout << "ERROR::PROGRAM_LINKING_ERROR of type: " << type << "\n" << infoLog << "\n -- --------------------------------------------------- -- " << std::endl;
-            }
-        }
-    }
+    void checkCompileErrors(GLuint shader, std::string type);
 };
 #endif /*__SHADER_H__*/
