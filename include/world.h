69f08e4589da4424b221ee9e115a4bbac0a110cc
Esma <esmazuurbier12@gmail.com>
2022-09-20 16:57:14 +0200

    edited world: no more ground, trees, glowstones objects

--- include/world.h
+++ include/world.h
@@ -7,66 +7,64 @@
 #ifndef __WORLD_H__
 #define __WORLD_H__
 
-#include "tree.h"
-#include "ground.h"
-#include "glowstone.h"
-#include "mobs.h"
-#include "vertex_data.h"
+#include "shader.h"
+#include "model.h"
+#include "skybox.h"
 
-class World {
+class World
+{
 public:
     // terrain settings
-    static const unsigned int N_GLOWSTONES;   // number of glowstones
-    static const unsigned int N_TREES;        // number of trees
-    static const unsigned int TERRAIN_SIZE;   // actual terrain size = TERRAIN_SIZE * TERRAIN_SIZE (it's a square)
-    static const unsigned int HEIGHT_TREE;    // height of tree (amount of blocks that make up a trunk)
-    static const float MIN_HEIGHT_MOB;        // minimum floating height of mobs
-    static const float MAX_HEIGHT_MOB;        // maximum floating height of mobs
-    static const float HEIGHT_GLOWSTONES;     // height of glow stone (counted from GROUND_Y + BLOCK_SIZE)
-    static const float BLOCK_SIZE;            // size of the blocks (initially they all have the same size)
-    static const float GROUND_Y;              // y level of ground
-    // objects & shaders
-    Ground ground;            // ground object
-    Tree trees;               // tree obect
-    GlowStone glowstones;     // glow stone object
-    Mobs mobs;                // mobs object
-    Shader shader;            // shader for all non-transparent blocks
-    Shader shaderTransparent; // shader for transparent blocks (only leaves)
+    static const unsigned int N_TREES;   // number of trees
+    static const float TERRAIN_SIZE;     // terrain size (it's a square)
+    static const float GROUND_Y;         // y level of ground
+    // shader related
+    glm::mat4 view;       // camera view matrix
+    glm::mat4 projection; // projection matrix
 
     /// constructs a new world object.
     World();
 
-    /**
-     * @brief draws the world (ground + trees + glow stones + mobs)
-     * 
-     * @param cameraView camera view matrix.
-     * @param projection projection matrix.
-     * @param currentTime current time/frame.
-     */
-    void Draw(glm::mat4 cameraView, glm::mat4 projection, float currentTime);
+    /// renders the terrain
+    void Draw();
+
+    /// returns the tree positions
+    std::vector<glm::vec3> getTreePositions() const;
 
 private:
-    // vertex data
-    std::vector< Vertex > trunkVertices;      // vertex data needed for rendering trunk blocks
-    std::vector< Vertex > leavesVertices;     // vertex data needed for rendering leaves blocks
-    std::vector< Vertex > dirtVertices;       // vertex data needed for rendering dirt blocks
-    std::vector< Vertex > glowStoneVertices;  // vertex data needed for rendering glow stone blocks
-    std::vector< Vertex > stoneVertices;      // vertex data needed for rendering stone blocks
-    std::vector< Vertex > creeperVertices;    // vertex data needed for rendering creeper head blocks
-    std::vector< Vertex > zombieVertices;     // vertex data needed for rendering zombie head blocks
-    // textures
-    std::string texturePath;      // path to where all the texture files are located
-    std::string pathTrunkDirtTex; // filename of trunk and dirt blocks texture 
-    std::string pathLeavesTex;    // filename of leaves block texture
-    std::string pathGlowStoneTex; // filename of glow stone block texture
-    std::string pathStoneTex;     // filename of stone block texture
-    std::string pathMobsTex;      // filename of mob block texture
-    
+    // objects & shaders
+    Shader shaderTree;   // tree model shader
+    Shader shaderGround; // ground shader
+    Shader shaderSkybox; // skybox shader
+    Model tree;          // tree model
+    SkyBox skybox;       // skybox
+    // object related
+    std::vector<glm::vec3> treePos;    // tree positions
+    std::vector<float> skyboxVertices; // skybox vertex data
+    std::vector<float> groundVertices; // ground vertex data
+    unsigned int groundTexture;
+    unsigned int groundVAO, groundVBO; // buffers for ground data
+
     /// sets up all the objects so that they can be rendered.
     void setupWorld();
 
-    /// sets up vertex data for all the blocks.
-    void setupData();
+    /// renders trees
+    void drawTrees();
+
+    /// renders ground
+    void drawGround();
+
+    /// renders skybox
+    void drawSkyBox();
+    
+    /// returns the skybox vertex data.
+    std::vector<float> getSkyboxVertexData();
+
+    /// returns vertex data for ground. Data includes positions and texture coords.
+    std::vector<float> getGroundVertexData();
+
+    /// creates N_TREES random positions.
+    void createTreePositions();
 };
 
 #endif
--- src/world.cpp
+++ src/world.cpp
@@ -1,81 +1,177 @@
 #include "world.h"
 
-const unsigned int World::N_GLOWSTONES = 8;   
-const unsigned int World::N_TREES = 15;        
-const unsigned int World::TERRAIN_SIZE = 30;  
-const unsigned int World::HEIGHT_TREE = 5;    
-const float World::HEIGHT_GLOWSTONES = 3.0f;  
-const float World::MIN_HEIGHT_MOB = 2.0f;
-const float World::MAX_HEIGHT_MOB = 6.0f;   
-const float World::BLOCK_SIZE = 1.0f;            
+const unsigned int World::N_TREES = 15;
+const float World::TERRAIN_SIZE = 30;
 const float World::GROUND_Y = -1.8f;
 
 World::World()
 {
-   setupWorld();
+    setupWorld();
 }
 
-void World::Draw(glm::mat4 cameraView, glm::mat4 projection, float currentTime)
+void World::Draw()
 {
-    trees.Draw(shader, shaderTransparent, cameraView, projection);
-    glowstones.Draw(shader, cameraView,projection);
-    ground.Draw(shader, cameraView, projection);
-    mobs.Spawn(shader, currentTime, cameraView, projection);
+    drawTrees();
+    drawGround();
+    drawSkyBox();
 }
 
 void World::setupWorld()
 {
-    // setup shaders
-    shader = Shader("shaders/blocks.vert", "shaders/blocks.frag");
-    shaderTransparent = Shader("shaders/blocks.vert", "shaders/transparent.frag");
+    // create shaders
+    shaderTree = Shader("shaders/model_loading.vert", "shaders/model_loading.frag");
+    shaderGround = Shader("shaders/ground.vert", "shaders/ground.frag");
+    shaderSkybox = Shader("shaders/skybox.vert", "shaders/skybox.frag");
 
-    // paths to the block textures
-    texturePath = "resources/textures/";
-    pathTrunkDirtTex = texturePath + "blocks.JPG";
-    pathLeavesTex = texturePath + "leaves.png";
-    pathGlowStoneTex = texturePath + "glowstone.jpg";
-    pathStoneTex = texturePath + "stone.jpg";
-    pathMobsTex = texturePath + "mobs.JPG";
+    // get ground vertex data and set up the buffers
+    groundVertices = getGroundVertexData();
+    glGenVertexArrays(1, &groundVAO);
+    glGenBuffers(1, &groundVBO);
+    glBindVertexArray(groundVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, groundVBO);
+    glBufferData(GL_ARRAY_BUFFER, groundVertices.size()*sizeof(float), &groundVertices[0], GL_STATIC_DRAW);
+    // positions
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *)0);
+    // texture coords
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *)(3 * sizeof(float)));
+    glBindVertexArray(0);
 
-    // setup vertex data
-    setupData();
+    // load ground texture
+    groundTexture = TextureFromFile("field.png", "resources/textures", false);
 
-    // setup objects
-    trees = Tree(trunkVertices, leavesVertices, pathTrunkDirtTex, pathLeavesTex, N_TREES, HEIGHT_TREE, TERRAIN_SIZE, GROUND_Y, BLOCK_SIZE);
-    ground = Ground(dirtVertices, stoneVertices, pathTrunkDirtTex, pathStoneTex, TERRAIN_SIZE, GROUND_Y);
-    mobs = Mobs(zombieVertices, creeperVertices, trees.treePositions, pathMobsTex, TERRAIN_SIZE, GROUND_Y, BLOCK_SIZE, MIN_HEIGHT_MOB, MAX_HEIGHT_MOB);
-    glowstones = GlowStone(glowStoneVertices, pathGlowStoneTex, trees.treePositions, N_GLOWSTONES, HEIGHT_GLOWSTONES);
+    // get skybox vertex data and initialize skybox object
+    skyboxVertices = getSkyboxVertexData();
+    std::vector<std::string> filenames = {"right.jpg", "left.jpg", "top.jpg", "bottom.jpg", "front.jpg", "back.jpg"};
+    std::string dirName = "resources/skybox";
+    skybox = SkyBox(skyboxVertices, filenames, dirName);
+
+    // load model
+    tree = Model("resources/models/trees/trees9.obj", true);
+
+    // generate tree positions
+    createTreePositions();
 }
 
-void World::setupData()
+void World::drawTrees()
 {
-    std::vector<float> positionData = getPositionData();       // position data for blocks
-    std::vector<float> textureCoords = getTextureCoordsData(); // texture coordinates for blocks
-
-    int indexPos = 0;
-    int indexTex = 0;
-    for (unsigned i = 0; i < getPositionData().size(); i++) // 36 lines of position data (all objects, except model(s) are just blocks)
+    // set uniforms for tree shader and draw the trees
+    shaderTree.use();
+    shaderTree.setMat4("projection", projection);
+    shaderTree.setMat4("view", view);
+    for (unsigned i = 0; i < World::N_TREES; i++)
     {
-        Vertex vertexTrunk, vertexLeaves, vertexDirt, vertexCreeper, vertexZombie;
-        // all blocks have the same positon data (leaves, glow stone and stone also have same texture coords data)
-        vertexTrunk.Position = vertexLeaves.Position = vertexDirt.Position = vertexCreeper.Position = vertexZombie.Position = glm::vec3(positionData[indexPos], positionData[indexPos + 1], positionData[indexPos + 2]);
-        // a line of texture coords data in textureCoords[] looks like: trunk.x, trunk.y, dirt.x, dirt.y, leaves.x, leaves.y (0, 1, 2, 3, 4, 5)
-        vertexTrunk.TexCoords = glm::vec2(textureCoords[indexTex], textureCoords[indexTex + 1]);
-        vertexLeaves.TexCoords = glm::vec2(textureCoords[indexTex + 4], textureCoords[indexTex + 5]);
-        vertexDirt.TexCoords = glm::vec2(textureCoords[indexTex + 2], textureCoords[indexTex + 3]);
-        vertexCreeper.TexCoords = glm::vec2(textureCoords[indexTex + 6], textureCoords[indexTex + 7]);
-        // zombie tex coords are almost the same as creeper tex coords, the only difference is that each y in zombie tex coords is 0.5f lower than y in creeper tex coords
-        vertexZombie.TexCoords = glm::vec2(textureCoords[indexTex + 6], textureCoords[indexTex + 7] - 0.5f); // subtract 0.5f from y value
-        // push data into vectors
-        trunkVertices.push_back(vertexTrunk);
-        leavesVertices.push_back(vertexLeaves);
-        dirtVertices.push_back(vertexDirt);
-        glowStoneVertices.push_back(vertexLeaves);
-        stoneVertices.push_back(vertexLeaves);
-        creeperVertices.push_back(vertexCreeper);
-        zombieVertices.push_back(vertexZombie);
-        // move to the next line in the arrays
-        indexPos += 3;
-        indexTex += 8;
+        glm::mat4 model = glm::mat4(1.0f);
+        model = glm::translate(model, treePos[i]);
+        model = glm::scale(model, glm::vec3(0.4f));
+        shaderTree.setMat4("model", model);
+        tree.drawSpecificMesh(shaderTree, 1);
+    }
+}
+
+void World::drawGround()
+{
+    // set uniforms for ground shader and draw the ground
+    shaderGround.use();
+    shaderGround.setMat4("projection", projection);
+    shaderGround.setMat4("view", view);
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, groundTexture);
+    glBindVertexArray(groundVAO);
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+
+    // set everything back to default
+    glBindVertexArray(0);
+    glActiveTexture(GL_TEXTURE0);
+}
+
+void World::drawSkyBox()
+{
+    // render skybox
+    glDepthFunc(GL_LEQUAL);
+    skybox.Draw(shaderSkybox, view, projection);
+    glDepthFunc(GL_LESS);
+}
+
+std::vector<float> World::getSkyboxVertexData()
+{
+    std::vector<float> v = {
+        // positions
+        -1.0f, 1.0f, -1.0f,
+        -1.0f, -1.0f, -1.0f,
+        1.0f, -1.0f, -1.0f,
+        1.0f, -1.0f, -1.0f,
+        1.0f, 1.0f, -1.0f,
+        -1.0f, 1.0f, -1.0f,
+
+        -1.0f, -1.0f, 1.0f,
+        -1.0f, -1.0f, -1.0f,
+        -1.0f, 1.0f, -1.0f,
+        -1.0f, 1.0f, -1.0f,
+        -1.0f, 1.0f, 1.0f,
+        -1.0f, -1.0f, 1.0f,
+
+        1.0f, -1.0f, -1.0f,
+        1.0f, -1.0f, 1.0f,
+        1.0f, 1.0f, 1.0f,
+        1.0f, 1.0f, 1.0f,
+        1.0f, 1.0f, -1.0f,
+        1.0f, -1.0f, -1.0f,
+
+        -1.0f, -1.0f, 1.0f,
+        -1.0f, 1.0f, 1.0f,
+        1.0f, 1.0f, 1.0f,
+        1.0f, 1.0f, 1.0f,
+        1.0f, -1.0f, 1.0f,
+        -1.0f, -1.0f, 1.0f,
+
+        -1.0f, 1.0f, -1.0f,
+        1.0f, 1.0f, -1.0f,
+        1.0f, 1.0f, 1.0f,
+        1.0f, 1.0f, 1.0f,
+        -1.0f, 1.0f, 1.0f,
+        -1.0f, 1.0f, -1.0f,
+
+        -1.0f, -1.0f, -1.0f,
+        -1.0f, -1.0f, 1.0f,
+        1.0f, -1.0f, -1.0f,
+        1.0f, -1.0f, -1.0f,
+        -1.0f, -1.0f, 1.0f,
+        1.0f, -1.0f, 1.0f
+        };
+
+    return v;
+}
+
+std::vector<float> World::getGroundVertexData()
+{
+    std::vector<float> v = {
+        // positions                                     // texcoords
+         TERRAIN_SIZE / 2, GROUND_Y,  TERRAIN_SIZE / 2,  TERRAIN_SIZE / 2, 0.0f,
+        -TERRAIN_SIZE / 2, GROUND_Y,  TERRAIN_SIZE / 2,  0.0f, 0.0f,
+        -TERRAIN_SIZE / 2, GROUND_Y, -TERRAIN_SIZE / 2,  0.0f, TERRAIN_SIZE / 2,
+
+         TERRAIN_SIZE / 2, GROUND_Y,  TERRAIN_SIZE / 2,  TERRAIN_SIZE / 2, 0.0f,
+        -TERRAIN_SIZE / 2, GROUND_Y, -TERRAIN_SIZE / 2,  0.0f, TERRAIN_SIZE / 2,
+         TERRAIN_SIZE / 2, GROUND_Y, -TERRAIN_SIZE / 2,  TERRAIN_SIZE / 2, TERRAIN_SIZE / 2
+        };
+
+    return v;
+}
+
+void World::createTreePositions()
+{
+    std::random_device rd;  // obtain a random number from hardware
+    std::mt19937 gen(rd()); // seed the generator
+
+    // create random positions
+    std::uniform_int_distribution<> xzPlane((-TERRAIN_SIZE / 2) + 1, (TERRAIN_SIZE / 2) - 1); // define the range for x and z axis
+    for (unsigned int i = 0; i < N_TREES; i++)
+    {
+        float posX = xzPlane(gen);
+        float posZ = xzPlane(gen);
+        glm::vec3 vec = {posX, GROUND_Y, posZ}; 
+        treePos.push_back(vec);
     }
 }
