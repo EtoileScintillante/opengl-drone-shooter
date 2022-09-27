bce62e3bce3cbe5f0596fb16263221ce29e9c084
Esma <esmazuurbier12@gmail.com>
2022-09-27 21:25:01 +0200

    improved calculation of bounding box

--- include/enemy.h
+++ include/enemy.h
@@ -8,6 +8,7 @@
 #ifndef __ENEMY_H__
 #define __ENEMY_H__
 
+#include <cmath>
 #include "world.h"
 #include "ray.h"
 #include "box.h"
@@ -40,10 +41,11 @@ public:
 private:
     glm::vec3 position;    // position of enemy
     glm::mat4 modelMatrix; // model matrix for enemy
+    AABBox boundingBox;    // enemy bounding box 
     bool isDead;           // is enemy dead?
     float deathTime;       // used to control the duration of the dying animation
     float magnitude;       // used to control the explosion (dying animation)
-    float rotation;        // used in the creation the model matrix for enemy
+    float rotation;        // degrees (used in the creation the model matrix for enemy)
 
     /// Spawns the enemy.
     void spawn();
@@ -65,6 +67,9 @@ private:
 
     /// Generate model matrix for enemy.
     void generateModelMatrix();
+
+    /// Calculates bounding box for enemy using the enemy's current position.
+    void calculateBoundingBox();
 };
 
 #endif /*__ENEMY__*/
--- src/enemy.cpp
+++ src/enemy.cpp
@@ -56,17 +56,16 @@ void Enemy::controlEnemyLife(bool shot, glm::vec3 bulletStartPos, glm::vec3 bull
 
 void Enemy::collisionDetection(glm::vec3 bulletStartPos, glm::vec3 bulletDir, float bulletRange)
 {
-    // comstruct ray object with start position and direction of bullet
+    // construct ray object with start position and direction of bullet
     glm::vec3 orig = bulletStartPos;
     glm::vec3 dir = bulletDir;
     Ray ray(orig, dir);
 
-    // construct bounding box object using the current position of the enemy
-    glm::vec3 vmin = {position.x - 1.0f, position.y - 0.5f, position.z - 0.5f};
-    glm::vec3 vmax = {position.x + 1.0f, position.y + 0.5f, position.z + 0.5f};
-    AABBox box(vmin, vmax);
+    // calculate bounding box
+    calculateBoundingBox();
 
-    if (box.intersect(ray, bulletRange) == true)
+    // check for collision
+    if (boundingBox.intersect(ray, bulletRange) == true)
     {
         isDead = true;
     }
@@ -115,6 +114,57 @@ void Enemy::generateModelMatrix()
     // create model matrix
     modelMatrix = glm::mat4(1.0f);
     modelMatrix = glm::translate(modelMatrix, position);
-    modelMatrix = glm::rotate(modelMatrix, rotation, glm::vec3(0.0f, 1.0f, 0.0f)); // add (semi)random rotation
+    modelMatrix = glm::rotate(modelMatrix, rotation, glm::vec3(0.0f, 1.0f, 0.0f)); // add (semi)random rotation (counter clockwise)
     modelMatrix = glm::scale(modelMatrix, glm::vec3(0.6f));
+}
+
+/* 
+To construct a bounding box we need two things: a minimum bound and a maximum bound.
+We also want the bounding box to rotate with the model.
+We can imagine the bounding box like a quadrilateral inscribed in a circle where the center is 
+the position of the enemy on the x z plane. The two bounds represent two points on that circle. 
+We don't actually need all 4 points to create a bounding box, the two min and max bounds are enough.
+If the model rotates N degrees, we also want those two points to rotate N degrees. 
+So we baically we rotate those two points (the bounds) about the center by N degrees. 
+The formula (for counter clockwise rotation) is as follows:
+
+newX = cos(θ) * (X - Cx) - sin(θ) * (Y - Cy) + Cx.
+newY = sin(θ) * (X - Cx) + cos(θ) * (Y - Cy) + Cy.
+
+Cx and Cy are the center coordinates, θ is the angle in radians, 
+X and Y are the initial coordinates and newX and newY are the rotated coordinates.
+
+Formula from https://math.stackexchange.com/a/814981.
+
+Important to note is that in this program the bounding box is shaped like a rectangle;
+the z sides are bigger than the x sides since the model of the enemy is longer than it is wide.
+Also, we don't do anything with the y coordinate, since the height of the box does not change 
+when rotating it around the y - axis. 
+*/
+
+void Enemy::calculateBoundingBox()
+{   
+    // center of circle
+    float Cx = position.x;
+    float Cz = position.z;
+
+    // initialize starting points/bounds
+    float initialMinX = Cx - 0.3f; // x of min bound
+    float initialMinZ = Cz - 1.0f; // z of min bound
+    float initialMaxX = Cx + 0.3f; // x of max bound
+    float initialMaxZ = Cz + 1.0f; // z of max bound
+
+    // convert degrees to radians (sin and cos functions expect radians)
+    float radians = (rotation * 3.14159265) / 180;
+
+    // calculate new points/bounds (note that the y from the formula is replaced by z)
+    float newMinX = (cos(radians) * (initialMinX - Cx)) - (sin(radians) * (initialMinZ - Cz)) + Cx;
+    float newMinZ = (sin(radians) * (initialMinX - Cx)) + (cos(radians) * (initialMinZ - Cz)) + Cz;
+    float newMaxX = (cos(radians) * (initialMaxX - Cx)) - (sin(radians) * (initialMaxZ - Cz)) + Cx;
+    float newMaxZ = (sin(radians) * (initialMaxX - Cx)) + (cos(radians) * (initialMaxZ - Cz)) + Cz;
+
+    // create bounding box with the new coordinates
+    glm::vec3 vmin = glm::vec3(newMinX, position.y, newMinZ);
+    glm::vec3 vmax = glm::vec3(newMaxX, position.y + 0.7f, newMaxZ);
+    boundingBox = AABBox(vmin, vmax);
 }
