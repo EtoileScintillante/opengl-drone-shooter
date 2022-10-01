057550ed72b0f90034eccaa56589672a5501609d
Esma <esmazuurbier12@gmail.com>
2022-10-01 17:27:07 +0200

    removed passive player motion control  + edited todo list

--- main.cpp
+++ main.cpp
@@ -2,7 +2,8 @@
 
 // TODO:
 // Make the drones dangerous; they should be able to kill the player in some way (maybe they can shoot bullets too?)
-// Add sound effects
+// Improve sound effect of drone (the closer the player is to the drone, the louder the hovering and explosion sound)
+// Add walking sound for player
 // Add scoring system
 // Maybe add small random offset to the bullet direction when player shoots gun
 
@@ -36,12 +37,14 @@ int main()
         currentFrame = static_cast<float>(glfwGetTime());
         deltaTime = currentFrame - lastFrame;
         lastFrame = currentFrame;
-        player.currentFrame = currentFrame; // pass time to player object
-        drone.currentFrame = currentFrame;  // also pass time variables to enemy object
-        drone.deltaTime = deltaTime; 
 
-        // movement when player is not moving (to make the player look more alive)
-        player.passiveMotion();
+        // pass time variables to player and enemy
+        player.currentFrame = currentFrame; 
+        drone.currentFrame = currentFrame; 
+        drone.deltaTime = deltaTime;
+
+        // pass player position to enemy, needed to control enemy soundeffects
+        drone.playerPosition = player.Position; 
 
         // input
         player.processInput(window, deltaTime);
@@ -52,7 +55,7 @@ int main()
         glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        // view and projection for world objects and enemy object
+        // view and projection for world objects and enemy
         world.projection = player.getProjectionMatrix();
         world.view = player.GetViewMatrix();
         drone.projection = player.getProjectionMatrix();
@@ -64,7 +67,7 @@ int main()
         // draw gun and handle gun recoil movement
         player.controlGunRendering();
         
-        // control life of drone (spawning and dying)
+        // control life of enemy (spawning and dying)
         drone.controlEnemyLife(player.shot, player.Position, player.Front, World::TERRAIN_SIZE * 2);
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
