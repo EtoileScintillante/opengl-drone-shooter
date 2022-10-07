3022135eccc32a50e7766167d77cbf789f9e795c
Esma <esmazuurbier12@gmail.com>
2022-10-07 15:18:15 +0200

    EnemyManager is back (solution was using shared ptrs)

---
+++ include/enemy_manager.h
@@ -0,0 +1,40 @@
+/**
+  * enemy_manager.h
+  * 
+  * Created by EtoileScintillante.
+  */
+
+#ifndef __ENEMY_MANAGER_H__
+#define __ENEMY_MANAGER_H__
+
+#include <memory>
+
+#include "enemy.h"
+#include "player.h"
+
+class EnemyManager
+{
+public:
+    static const int MAX_ENEMIES; // max number of enemies that can exist in the game
+    static const float INTERVAL;  // time in seconds between spawning of new enemies
+    float currentTime;            // current time/frame
+    float deltaTime;              // time passed between two frames
+
+    /// Construct a Enemy Manager object. This also initializes the enemy objects.
+    EnemyManager();
+
+    /**
+     * @brief Manages the enemy objects. 
+     * 
+     * @param player player object.
+     * @param bulletRange range of bullet (maximum distance the bullet can travel).
+     */
+    void manage(Player &player, float bulletRange);
+
+private:
+    std::vector<std::shared_ptr<Enemy>> enemies; // vector containing pointers to the enemy objects
+    float spawnTime;                             // needed to control spawning of enemies
+    int enemyCount;                              // keeps track of already spawned enemies
+};
+
+#endif /*__ENEMY_MANAGER__*/
---
+++ src/enemy_manager.cpp
@@ -0,0 +1,37 @@
+#include "enemy_manager.h"
+
+const int EnemyManager::MAX_ENEMIES = 3;
+const float EnemyManager::INTERVAL = 3.0f;
+
+EnemyManager::EnemyManager()
+{
+    // set default values
+    enemyCount = 1; // always start with at least one enemy
+    spawnTime = 0;
+
+    // initialize enemies
+    for (unsigned int i = 0; i < MAX_ENEMIES; i++)
+    {
+        enemies.push_back(std::make_shared<Enemy>());
+    }
+}
+
+void EnemyManager::manage(Player &player, float bulletRange)
+{
+    if (enemyCount < MAX_ENEMIES + 1) // enemyCount starts at 1
+    {
+        spawnTime += deltaTime;
+        if (spawnTime >= INTERVAL)
+        {
+            enemyCount++;
+            spawnTime = 0;
+        }
+    }
+    
+    for (unsigned int i = 0; i < enemyCount - 1; i++)
+    {
+        enemies[i]->currentFrame = currentTime;
+        enemies[i]->deltaTime = deltaTime;
+        enemies[i]->controlEnemyLife(player, bulletRange);
+    }
+}
