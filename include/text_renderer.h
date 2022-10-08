42ecf7780bbfe90e9062e3bc1141f90ea1343579
Esma <esmazuurbier12@gmail.com>
2022-10-08 17:58:15 +0200

    added class TextRenderer

---
+++ include/text_renderer.h
@@ -0,0 +1,69 @@
+/**
+ * text_renderer.h
+ *
+ * Created by EtoileScintillante.
+ * Based on code from Joey de Vries (https://learnopengl.com/In-Practice/Text-Rendering).
+ */
+
+#ifndef __TEXT_RENDERER_H__
+#define __TEXT_RENDERER_H__
+
+#include <string>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include "player.h"
+#include "shader.h"
+
+#include <ft2build.h>
+#include FT_FREETYPE_H
+
+/// Holds all state information relevant to a character as loaded using FreeType.
+struct Character
+{
+    unsigned int TextureID; // ID handle of the glyph texture
+    glm::ivec2 Size;        // Size of glyph
+    glm::ivec2 Bearing;     // Offset from baseline to left/top of glyph
+    unsigned int Advance;   // Horizontal offset to advance to next glyph
+};
+
+class TextRenderer
+{
+public:
+    /// Constructs new TextRenderer object.
+    /// Also initializes FreeType library, loads font, compiles shaders and sets projection matrix.
+    TextRenderer(std::string pathToFont, std::string pathVertexShader, std::string pathFragmentShader);
+
+    /**
+     * @brief Renders a line of text. Bottom left corner of screen is (x,y) = (0,0).
+     *
+     * @param text text (string) to render.
+     * @param x x position of text (of the first character).
+     * @param y y position of text.
+     * @param scale scale factor.
+     * @param color color of text.
+     */
+    void RenderText(std::string_view text, float x, float y, float scale, glm::vec3 color);
+
+private:
+    std::string font;          // path to font
+    glm::mat4 orthoProjection; // ortho projection matrix (no need for perspective when rendering text)
+    FT_Library ft;             // FreeType Library
+    FT_Face face;              // needed to load font as a face
+    Character characters[95];  // covers ASCII values from 32 to 127
+    Shader shader;             // shader
+    unsigned int VBO, VAO;     // data buffers
+
+    /// Loads font as face.
+    void loadFace();
+
+    /// Configures buffers for texture quads.
+    void configBuffers();
+};
+
+#endif /*__TEXT_RENDERER__*/
---
+++ src/text_renderer.cpp
@@ -0,0 +1,155 @@
+#include "text_renderer.h"
+
+TextRenderer::TextRenderer(std::string pathToFont, std::string pathVertexShader, std::string pathFragmentShader)
+{
+    this->font = pathToFont;
+
+    // projection matrix
+    orthoProjection = glm::ortho(0.0f, static_cast<float>(Player::SCR_WIDTH), 0.0f, static_cast<float>(Player::SCR_HEIGHT));
+
+    // compile shaders
+    shader = Shader(pathVertexShader.c_str(), pathFragmentShader.c_str());
+
+    // set projection matrix (does not change, so it's done here instead of in each frame)
+    shader.use();
+    glUniformMatrix4fv(glGetUniformLocation(shader.ID, "projection"), 1, GL_FALSE, glm::value_ptr(orthoProjection));
+
+    // All functions return a value different than 0 whenever an error occurred
+    if (FT_Init_FreeType(&ft))
+    {
+        std::cout << "ERROR::FREETYPE: Could not init FreeType Library" << std::endl;
+    }
+
+    // find path to font
+    if (font.empty())
+    {
+        std::cout << "ERROR::FREETYPE: Failed to load font" << std::endl;
+    }
+
+    // load font as face
+    loadFace();
+
+    // configure VAO and VBO
+    configBuffers();
+}
+
+void TextRenderer::RenderText(std::string_view text, float x, float y, float scale, glm::vec3 color)
+{
+    // set OpenGL state
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+    // activate corresponding render state
+    shader.use();
+    glUniform3f(glGetUniformLocation(shader.ID, "textColor"), color.x, color.y, color.z);
+    glActiveTexture(GL_TEXTURE0);
+    glBindVertexArray(VAO);
+
+    // iterate through all characters
+    std::string_view::const_iterator c;
+    for (c = text.begin(); c != text.end(); c++)
+    {
+        const Character ch = characters[*c - 32];
+
+        float xpos = x + ch.Bearing.x * scale;
+        float ypos = y - (ch.Size.y - ch.Bearing.y) * scale;
+
+        float w = ch.Size.x * scale;
+        float h = ch.Size.y * scale;
+        // update VBO for each character
+        float vertices[6][4] = {
+            {xpos, ypos + h, 0.0f, 0.0f},
+            {xpos, ypos, 0.0f, 1.0f},
+            {xpos + w, ypos, 1.0f, 1.0f},
+
+            {xpos, ypos + h, 0.0f, 0.0f},
+            {xpos + w, ypos, 1.0f, 1.0f},
+            {xpos + w, ypos + h, 1.0f, 0.0f}};
+        // render glyph texture over quad
+        glBindTexture(GL_TEXTURE_2D, ch.TextureID);
+        // update content of VBO memory
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices); // be sure to use glBufferSubData and not glBufferData
+
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        // render quad
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+        // now advance cursors for next glyph (note that advance is number of 1/64 pixels)
+        x += (ch.Advance >> 6) * scale; // bitshift by 6 to get value in pixels (2^6 = 64 (divide amount of 1/64th pixels by 64 to get amount of pixels))
+    }
+    // reset
+    glBindVertexArray(0);
+    glBindTexture(GL_TEXTURE_2D, 0);
+    glDisable(GL_BLEND);
+}
+
+void TextRenderer::loadFace()
+{
+    if (FT_New_Face(ft, font.c_str(), 0, &face))
+    {
+        std::cout << "ERROR::FREETYPE: Failed to load font as face" << std::endl;
+    }
+    else
+    {
+        // set size to load glyphs as
+        FT_Set_Pixel_Sizes(face, 0, 48);
+
+        // disable byte-alignment restriction
+        glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+
+        // load first 128 characters of ASCII set
+        for (unsigned char c = 32; c < 127; c++)
+        {
+            // Load character glyph
+            if (FT_Load_Char(face, c, FT_LOAD_RENDER))
+            {
+                std::cout << "ERROR::FREETYTPE: Failed to load Glyph" << std::endl;
+                continue;
+            }
+            // generate texture
+            unsigned int texture;
+            glGenTextures(1, &texture);
+            glBindTexture(GL_TEXTURE_2D, texture);
+            glTexImage2D(
+                GL_TEXTURE_2D,
+                0,
+                GL_RED,
+                face->glyph->bitmap.width,
+                face->glyph->bitmap.rows,
+                0,
+                GL_RED,
+                GL_UNSIGNED_BYTE,
+                face->glyph->bitmap.buffer);
+            // set texture options
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+            // now store character for later use
+            Character character = {
+                texture,
+                glm::ivec2(face->glyph->bitmap.width, face->glyph->bitmap.rows),
+                glm::ivec2(face->glyph->bitmap_left, face->glyph->bitmap_top),
+                static_cast<unsigned int>(face->glyph->advance.x)};
+            characters[c - 32] = character;
+        }
+        glBindTexture(GL_TEXTURE_2D, 0);
+    }
+    // destroy FreeType when finished
+    FT_Done_Face(face);
+    FT_Done_FreeType(ft);
+}
+
+void TextRenderer::configBuffers()
+{
+    // each 2D quad requires 6 vertices of 4 floats each
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 6 * 4, NULL, GL_DYNAMIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(float), 0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+}
