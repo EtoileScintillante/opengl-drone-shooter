241cd620da592983535ea0c37476d61e8c34a5d5
Esma <esmazuurbier12@gmail.com>
2022-10-12 17:30:14 +0200

    edited docs and endingScreen funct

--- include/hud.h
+++ include/hud.h
@@ -9,44 +9,26 @@
 #define __HUD_H__
 
 #include "text_renderer.h"
+#include "enemy_manager.h"
 #include "player.h"
 #include "world.h"
 
-/// @brief Renders simple starting screen (with instruction on how to move).
-/// @param tr TextRenderer object.
-/// @param player player object.
+/// @brief Renders simple start/title screen.
+/// @param[out] tr TextRenderer object.
+/// @param[out] player Player object.
+/// @param[out] world World object (for drawing skybox).
 void startingScreen(TextRenderer &tr, Player &player, World &world);
 
 /// @brief Renders text that should be visible while playing: player's kills and health.
-/// @param tr TextRenderer object.
-/// @param player player object.
+/// @param[out] tr TextRenderer object.
+/// @param[out] player Player object.
 void inGameScreen(TextRenderer &tr, Player &player);
 
 /// @brief Rneders simple ending screen (with option to play again).
-/// @param tr TextRenderer object.
-/// @param player player object.
-void endingScreen(TextRenderer &tr, Player &player, World &world);
-
-/*
-TODO:
-- starting HUD (black text in middle of screen, render only skybox)
-  "Press SPACE to start"
-  "Movement:
-  "[W] = forward"
-  "[S] = backward"
-  "[D] = right"
-  "[A] = left"
-  "[SPACE] = shoot"
-- in game HUD (black text, bottom left corner; see implementation in main loop)
-- game over (player died) HUD (black text in middle of screen, render only skybox)
-  "Game Over!"
-  "Kills: [player.kills]"
-  "Press [SPACE] to play again"
-  "Press [ESC] to quit"
-
-Notes: to control starting/ending of game, use a global variable in main "started" and use player.isAlive. 
-Also if after game over the player presses space, we need to reset the player's values, 
-this can easily be achieved by adding a "reset" method to the player class
-*/
+/// @param[out] tr TextRenderer object.
+/// @param[out] player Player object.
+/// @param[out] world WWorld object (for drawing skybox).
+/// @param[out] em EnemyManager object (will be reset).
+void endingScreen(TextRenderer &tr, Player &player, World &world, EnemyManager &em);
 
 #endif /*__HUD__*/
