3dc445eb522c73a363f76a23129ccebe26834b5e
Esma <esmazuurbier12@gmail.com>
2022-10-12 14:39:18 +0200

    centered text

--- src/hud.cpp
+++ src/hud.cpp
@@ -12,8 +12,8 @@ void startingScreen(TextRenderer &tr, Player &player, World &world)
 
     // render title screen (scales and positions based on theboldfont.ttf)
     tr.RenderText("Drone  Shooter", 75.0f, 400.0f, 1.8f, glm::vec3(0.0f, 0.0f, 0.0f));
-    tr.RenderText("Press  ENTER  to  start", 240.0f, 303.0f, 0.55f, glm::vec3(1.0f, 1.0f, 1.0f));
-    tr.RenderText("by  Esma", 350.0f, 30.0f, 0.4f, glm::vec3(1.0f, 1.0f, 1.0f));
+    tr.RenderText("Press  ENTER  to  start", 253.0f, 303.0f, 0.55f, glm::vec3(1.0f, 1.0f, 1.0f));
+    tr.RenderText("by  Esma", 352.0f, 30.0f, 0.4f, glm::vec3(1.0f, 1.0f, 1.0f));
 }
 
 void inGameScreen(TextRenderer &tr, Player &player)
